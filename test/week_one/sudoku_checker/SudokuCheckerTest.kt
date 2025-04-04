3f1c29ee6444ab5bfcc1b66b6142948509fda0a3
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-04-04 01:17:49 +0000

    refactor test cases related to the Sudoku Puzzle

--- test/week_one/sudoku_checker/SudokuCheckerTest.kt
+++ test/week_one/sudoku_checker/SudokuCheckerTest.kt
@@ -3,55 +3,55 @@ package week_one.sudoku_checker
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeEmptyFullyInvValidSudokuPuzzle
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeEmptyPartiallyInvValidSudokuPuzzle
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeInvalidColumnSudokuPuzzle
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeInvalidRowSudokuPuzzle
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeInvalidSubGridSudokuPuzzle
-import week_one.sudoku_checker.fakes.FakeDataSource.fakeInvalidationsSudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeDuplicateInColumnSudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeDuplicateInRowSudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeDuplicateInSubGridSudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeEmptyFullSudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeEmptyPartiallySudokuPuzzle
+import week_one.sudoku_checker.fakes.FakeDataSource.fakeNonAllowedCharsInSudokuPuzzle
 import week_one.sudoku_checker.fakes.FakeDataSource.fakeValidSudokuPuzzle
 
 class SudokuCheckerTest {
 
  @Test
- fun `test success checkSudokuPuzzle, returns true`() {
+ fun `should return true, when Sudoku puzzle is valid`() {
   val expectedResult = checkSudokuPuzzle(fakeValidSudokuPuzzle)
   assertTrue(expectedResult)
  }
 
  @Test
- fun `test failure checkSudokuPuzzle fully empty puzzle, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeEmptyFullyInvValidSudokuPuzzle)
+ fun `should return true, when Sudoku puzzle is fully empty`() {
+  val expectedResult = checkSudokuPuzzle(fakeEmptyFullSudokuPuzzle)
+  assertTrue(expectedResult)
+ }
+
+ @Test
+ fun `should return true, when Sudoku puzzle is partially empty`() {
+  val expectedResult = checkSudokuPuzzle(fakeEmptyPartiallySudokuPuzzle)
+  assertTrue(expectedResult)
+ }
+
+ @Test
+ fun `should return false, when Sudoku puzzle has non allowed chars`() {
+  val expectedResult = checkSudokuPuzzle(fakeNonAllowedCharsInSudokuPuzzle)
   assertFalse(expectedResult)
  }
 
  @Test
- fun `test failure checkSudokuPuzzle partially empty puzzle, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeEmptyPartiallyInvValidSudokuPuzzle)
+ fun `should return false, when Sudoku puzzle has two or more duplicates in row`() {
+  val expectedResult = checkSudokuPuzzle(fakeDuplicateInRowSudokuPuzzle)
   assertFalse(expectedResult)
  }
 
  @Test
- fun `test failure checkSudokuPuzzle with repeated row, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeInvalidRowSudokuPuzzle)
+ fun `should return false, when Sudoku puzzle has two or more duplicates in column`() {
+  val expectedResult = checkSudokuPuzzle(fakeDuplicateInColumnSudokuPuzzle)
   assertFalse(expectedResult)
  }
 
  @Test
- fun `test failure checkSudokuPuzzle with repeated column, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeInvalidColumnSudokuPuzzle)
-  assertFalse(expectedResult)
- }
-
- @Test
- fun `test failure checkSudokuPuzzle with repeated subgrid, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeInvalidSubGridSudokuPuzzle)
-  assertFalse(expectedResult)
- }
-
- @Test
- fun `test failure checkSudokuPuzzle with multiple invalidations, returns false`() {
-  val expectedResult = checkSudokuPuzzle(fakeInvalidationsSudokuPuzzle)
+ fun `should return false, when Sudoku puzzle has two or more duplicates in subgrid`() {
+  val expectedResult = checkSudokuPuzzle(fakeDuplicateInSubGridSudokuPuzzle)
   assertFalse(expectedResult)
  }
 

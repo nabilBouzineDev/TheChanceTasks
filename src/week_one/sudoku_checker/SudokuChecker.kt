8324744aa27cdeaf18513e09a2fd5d85b0d48099
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-04-04 01:17:49 +0000

    refactor test cases related to the Sudoku Puzzle

--- src/week_one/sudoku_checker/SudokuChecker.kt
+++ src/week_one/sudoku_checker/SudokuChecker.kt
@@ -5,6 +5,7 @@ package week_one.sudoku_checker
  * 1. No repeating values in the row.
  * 2. No repeating values in the column.
  * 3. No repeating values in subgrid of 3x3.
+ * 4. Only numbers from 1 to 9 are allowed
  *
  * Otherwise, it's invalid.
  */
--- test/week_one/sudoku_checker/fakes/FakeDataSource.kt
+++ test/week_one/sudoku_checker/fakes/FakeDataSource.kt
@@ -6,41 +6,82 @@ object FakeDataSource {
         charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
         charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
         charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
+        charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+        charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+        charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+        charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+        charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+        charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
     )
 
-    val fakeEmptyFullyInvValidSudokuPuzzle = arrayOf(
+    val fakeEmptyFullSudokuPuzzle = arrayOf(
         charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
         charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
         charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
+        charArrayOf('-', '-', '-', '-', '-', '-', '-', '-', '-')
     )
 
-    val fakeEmptyPartiallyInvValidSudokuPuzzle = arrayOf(
+    val fakeEmptyPartiallySudokuPuzzle = arrayOf(
         charArrayOf('5', '3', '-', '-', '7', '-', '-', '-', '-'),
         charArrayOf('6', '-', '-', '1', '9', '5', '-', '-', '-'),
         charArrayOf('-', '9', '8', '-', '-', '-', '-', '6', '-'),
+        charArrayOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
+        charArrayOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
+        charArrayOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
+        charArrayOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
+        charArrayOf('-', '-', '-', '4', '1', '9', '-', '-', '5'),
+        charArrayOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
     )
 
-    val fakeInvalidRowSudokuPuzzle = arrayOf(
+    val fakeNonAllowedCharsInSudokuPuzzle = arrayOf(
+        charArrayOf('@', '3', '-', '-', '7', '-', '-', '-', '-'), // invalid character in this row '@'
+        charArrayOf('6', '-', '-', '1', '9', '5', '-', '-', '-'),
+        charArrayOf('-', '9', '8', '-', '-', '-', '-', '0', '-'), // invalid char in this row '0'
+        charArrayOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
+        charArrayOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
+        charArrayOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
+        charArrayOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
+        charArrayOf('-', '-', '-', '4', '1', '9', '-', '-', '5'),
+        charArrayOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
+    )
+    val fakeDuplicateInRowSudokuPuzzle = arrayOf(
         charArrayOf('5', '3', '4', '6', '7', '4', '9', '1', '2'), // invalid in first row with two 4s
         charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
         charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
+        charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+        charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+        charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+        charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+        charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+        charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
     )
 
-    val fakeInvalidColumnSudokuPuzzle = arrayOf(
+    val fakeDuplicateInColumnSudokuPuzzle = arrayOf(
         charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'), // invalid in the second column with two 7s
         charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
         charArrayOf('1', '7', '8', '3', '4', '2', '5', '6', '7'),
+        charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+        charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+        charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+        charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+        charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+        charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
     )
 
-    val fakeInvalidSubGridSudokuPuzzle = arrayOf(
+    val fakeDuplicateInSubGridSudokuPuzzle = arrayOf(
         charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'), // invalid in the first subgrid with two 5s and 4s
         charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
         charArrayOf('4', '9', '5', '3', '4', '2', '5', '6', '7'),
-    )
-
-    val fakeInvalidationsSudokuPuzzle = arrayOf(
-        charArrayOf('5', '3', '4', '6', '7', '4', '9', '1', '2'), // All the above invalidation.
-        charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-        charArrayOf('4', '7', '5', '3', '4', '2', '5', '6', '7'),
+        charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+        charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+        charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+        charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+        charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+        charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
     )
 }
